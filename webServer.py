171e5b7cade5f691999097d50285cdd1c1f44786
coltonthompson-nyu <cjt9861@nyu.edu>
2024-09-12 01:20:54 -0400

    first attempt

--- webServer.py
+++ webServer.py
@@ -2,6 +2,8 @@
 from socket import *
 # In order to terminate the program
 import sys
+from socket import *
+import sys
 
 
 
@@ -12,6 +14,7 @@ def webServer(port=13331):
   serverSocket.bind(("", port))
   
   #Fill in start
+  serverSocket.listen()
 
   #Fill in end
 
@@ -19,15 +22,23 @@ def webServer(port=13331):
     #Establish the connection
     
     print('Ready to serve...')
-    connectionSocket, addr = #Fill in start -are you accepting connections?     #Fill in end
+    connectionSocket, addr = serverSocket.accept() #Fill in start -are you accepting connections?     #Fill in end
     
     try:
-      message = #Fill in start -a client is sending you a message   #Fill in end 
+      message = connectionSocket.recv(1024).decode() #Fill in start -a client is sending you a message   #Fill in end
       filename = message.split()[1]
-      
+      #TEST
+      #print(message)
+      #END TEST
+
       #opens the client requested file. 
       #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:], #fill in start #fill in end)
+      f = open(filename[1:])#,  #fill in start #fill in end)
+
+      #TODO add in filename logic
+      if filename == "/helloworld.html":
+        f = open(filename[1:], "r")
+        outputdata = f.read()
       #fill in end
       
 
@@ -35,22 +46,31 @@ def webServer(port=13331):
       #Fill in start 
               
       #Content-Type is an example on how to send a header as bytes. There are more!
-      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
+      #outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
 
+        connectionSocket.send("HTTP/1.1 200 OK\r\n".encode())
+        connectionSocket.send("Content-Type: text/html; charset=UTF-8\r\n".encode())
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
- 
+        connectionSocket.send("\r\n\r\n".encode())
       #Fill in end
                
-      for i in f: #for line in file
-      #Fill in start - append your html file contents #Fill in end 
-        
+        #for i in f: #for line in file
+        for i in range(0, len(outputdata)):
+          connectionSocket.send(outputdata[i].encode())
+      #Fill in start - append your html file contents #Fill in end
+        connectionSocket.send(b"\r\n")
+        f.close()
       #Send the content of the requested file to the client (don't forget the headers you created)!
       #Send everything as one send command, do not send one line/item at a time!
 
       # Fill in start
-
-
+      else:
+        # this seems to be working correctly
+        print("404 Not Found")
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>\r\n".encode())
       # Fill in end
         
       connectionSocket.close() #closing the connection socket
@@ -59,13 +79,20 @@ def webServer(port=13331):
       # Send response message for invalid request due to the file not being found (404)
       # Remember the format you used in the try: block!
       #Fill in start
+      #print("exception occured")
+      #print(e)
+
+      #print("404 Not Found")
+      connectionSocket.send("HTTP/1.1 404 Not Found\r\n".encode())
+      connectionSocket.send("\r\n".encode())
+      connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>\r\n".encode())
 
       #Fill in end
 
 
       #Close client socket
       #Fill in start
-
+      connectionSocket.close()
       #Fill in end
 
   # Commenting out the below (some use it for local testing). It is not required for Gradescope, and some students have moved it erroneously in the While loop. 
